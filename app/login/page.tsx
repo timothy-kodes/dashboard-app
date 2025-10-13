313be6b921ad0fe001ea41b0109a16e849a35c94
Timothy Kodes <me@timothykodes.com>
2025-10-13 15:48:33 +0200

    add standard metadata

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,11 @@
+import type { Metadata } from 'next'
 import AcmeLogo from '@/app/ui/acme-logo'
 import LoginForm from '@/app/ui/login-form'
 import { Suspense } from 'react'
+
+export const metadata: Metadata = {
+  title: 'Login',
+}
 
 export default function LoginPage() {
   return (
